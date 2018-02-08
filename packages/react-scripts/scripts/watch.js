b508a8576988146e8e0459441d88ef4dd7496ebf
Truong Pham <truongpxc@gmail.com>
2018-02-08 12:28:03 +0700

    Fix config path

--- packages/react-scripts/scripts/watch.js
+++ packages/react-scripts/scripts/watch.js
@@ -9,5 +9,8 @@ const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
+const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath);
 // Now that we have packed them, call the global CLI.
-cp.execSync("cross-env NODE_ENV=development ../config/webpack.config.dev.js --watch --progress --hide-modules");
+
+var command = "cross-env NODE_ENV=development " + resolveOwn('config/webpack.config.dev.js') + " --watch --progress --hide-modules";
+cp.execSync(command);
