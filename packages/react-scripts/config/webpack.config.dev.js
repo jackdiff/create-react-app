7162595ebb1992ee33b38371f03253f2e09455f2
Truong Pham <truongpxc@gmail.com>
2018-03-29 11:17:05 +0700

    Fix css build dev

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -270,6 +270,9 @@ module.exports = {
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
     new webpack.DefinePlugin(env.stringified),
+    new ExtractTextPlugin({
+      filename: cssFilename,
+    }),
     // This is necessary to emit hot updates (currently CSS only):
     new webpack.HotModuleReplacementPlugin(),
     // Watcher doesn't work well if you mistype casing in a path so we use
